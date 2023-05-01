cf18db27e4ea05ba380533d006f8fac5f90dea65
ALEEF02 <thesmarty2002@gmail.com>
2023-05-01 14:39:37 -0400

    Mark the runner here as outdated. Move to https://github.com/StevensRockSat-C-2022-23/AIR

--- runner.py
+++ runner.py
@@ -1,14 +1,29 @@
+# THIS FILE IS OUT OF DATE! MIGRATE TO https://github.com/StevensRockSat-C-2022-23/AIR
+
+# Settings
+DEFAULT_BOOT_TIME = 35000   # The estimated time to boot and run the beginnings of the script, in MS. Will be used only if RTC is not live
+
 from ai_benchmark import AIBenchmark
-import psutil, os
+import psutil, os, time
 
 def main():
-    p = psutil.Process(os.getpid())
-    p.nice(-15) # Set this process to a higher priority. Could help us with GPU Memory issues
+    try:
+        p = psutil.Process(os.getpid())
+        p.nice(-15) # Set this process to a higher priority. Could help us with GPU Memory issues
+    except:
+        print("Couldn't elevate process!! Are we not running as sudo?")
     
     aib = AIBenchmark(verbose_level=2)
-    #results = aib.run_micro() # Inference and Micro take 2GB of RAM while Training takes 4GB
+    
+    while get_uptime() < 120 - (DEFAULT_BOOT_TIME / 1000):
+        pass
+    
     results = aib.run_nano()
+
+def get_uptime():
+    return time.time() - psutil.boot_time()
 
 if __name__ == "__main__":
     # execute only if run as a script
     main()
+
